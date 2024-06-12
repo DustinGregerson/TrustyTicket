893e312c6ac82a1d1c51bb835f1dc5181b0f29fe
Dustin Gregerson <DustinGregerson4@gmail.com>
2024-06-12 01:51:06 -0500

    Added first name, last name, and state select input fields for account register. adjusted css slightly and added a php if to switch the bodys class if the client is within the hub page or not.

--- header.php
+++ header.php
@@ -8,11 +8,15 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
+<!--Hub-->
 <?php if($_GET["action"]=="hub"):?>
     <body class="hub">
 <?php else:?>
     <body>
 <?php endif;?>
+
+<!--After Login-->
+<?php if(isset($_SESSION["user_id"])):?>
 <div class="container">
         <nav class="sidebar">
             <div class="sidebar-header">
@@ -24,10 +28,21 @@
                 <li><a href="#manage-events">Manage Events</a></li>
                 <li><a href="#tickets">Tickets</a></li>
                 <li><a href="#payment">Payment</a></li>
+                <li><a href="?action=logout">Log Out</a></li>
             </ul>
             <div class="host-rating">
                 <h1>Your Host Rating: <span id="host-rating">0-10</span></h1>
             </div>
         </nav>
+        
+<?php else:?>
+
+    <header>
+        <div class="navbar">
+            <div class="logo"><a href="">TrustyTicket</a></div>
+        </div>
+    </header>
+
+<?php endif;?>
 
 
